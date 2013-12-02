c50533155325a7b92691ed74a917b10d61d38563
Tim Payne <tim.payne@reachlocal.com>
2013-12-02 15:19:11 -0800

    Make `pra help` more helpful
    Added a reference to the correct help commands to use for details on
    the repo and source commands.

--- lib/pra/cli.rb
+++ lib/pra/cli.rb
@@ -7,16 +7,16 @@ require 'highline/import'
 module Pra
   module Cli
     class Main < Thor
+      register Pra::Cli::Repo, :repo, "repo SUBCOMMAND", "Manage repositories. See `pra repo help` for details."
+      register Pra::Cli::Source, :source, "source SUBCOMMAND", "Manage pull sources. See `pra source help` for details."
+
       default_task :launch
       desc "launch", "Start pra"
       def launch
         Pra::App.new.run
       end
 
-      desc "source", "source SUBCOMMAND"
       subcommand "source", Pra::Cli::Source
-
-      desc "repo", "repo SUBCOMMAND"
       subcommand "repo", Pra::Cli::Repo
     end
   end
